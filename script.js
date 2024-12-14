458c6a41fca31c2ec131c85af61f68ecb012b5e8
ericafk0001 <dxslyofficial@gmail.com>
2024-12-13 19:05:47 -0500

    correct comment

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("page loaded");
-
   const inputText = document.getElementById("input-text");
   const outputText = document.getElementById("output-text");
-
   const huzz = "uzz";
-
   inputText.addEventListener("input", () => {
     const words = inputText.value.split(" ");
-
     const newWords = words.map((word) => {
       if (word.length > 2) {
-        // Remove the last 2 characters and append "uzz"
+        // Remove the last 1 characters and append "uzz"
         return word.slice(0, -1) + huzz;
       } else {
         return word;
